b954faf34a9a3452e443c4d4f995c42461fb6f1f
BellZhao1101 <128668916+BellZhao1101@users.noreply.github.com>
2024-04-05 22:12:25 +1100

    version 2.1

--- main_100gb_pv1.py
+++ main_100gb_pv1.py
@@ -12,19 +12,20 @@ size = comm.Get_size()
 start_time = time.time()
 
 ## 用dict 存，用array 存可能会更快
+# start 改
 day_sentiment_dict  = {}
 hour_sentiment_dict  = {}
 day_count_dict = {}
 hour_count_dict = {}
-
-total_bytes = os.path.getsize("twitter-50mb.json")
+# end  改
+total_bytes = os.path.getsize("twitter-100gb.json")
 bytes_per_node = total_bytes // size
 bytes_already_read = 0
 position_start = rank * bytes_per_node
 count = 0
 
 # 读取的方法也可以多写
-with open("twitter-50mb.json", 'r') as file:
+with open("ttwitter-100gb.json", 'r') as file:
 
     file.seek(position_start,0)
 
@@ -36,12 +37,14 @@ with open("twitter-50mb.json", 'r') as file:
             count += 1
             day, hour = extract_datetime(row)
             sentiment = extract_sentiment(row)
+            # start 改
             if day and hour is not None:
                 day_count_dict[day] = day_count_dict.get(day, 0) + 1
                 hour_count_dict[(day,hour)] = hour_count_dict.get((day,hour), 0) + 1
                 if sentiment is not None:
                     day_sentiment_dict[day] = day_sentiment_dict.get(day, 0) +  sentiment
                     hour_sentiment_dict[(day,hour)] = hour_sentiment_dict.get((day,hour), 0) +  sentiment
+            # start 改
         bytes_already_read +=  len(row.encode('utf-8')) + 1 
         
         if bytes_already_read >= bytes_per_node:
@@ -52,17 +55,18 @@ with open("twitter-50mb.json", 'r') as file:
 
 # print("My rank is",rank, "My count is", count)
 
-
+# start 改 （gather的话不用改）
 hd_result_list = comm.gather([day_sentiment_dict], root=0)
 hh_result_list = comm.gather([hour_sentiment_dict], root=0)
 ad_result_list = comm.gather([day_count_dict], root=0)
 ah_result_list = comm.gather([hour_count_dict], root=0)
+# end 改
 
-
-# print_time(start_time, "Parallel Time (gather data): ")
-print_time(start_time, "Parallel Time (gather data): ")
+print_time(start_time, "Parallel Time (read and gather) : ")
 
 if rank == 0:
+    start_time = time.time()
+    # merge and find max 的 function 改
     day_active, max_day_active = merge_and_find_max(ad_result_list)
     hour_active, max_hour_active = merge_and_find_max(ah_result_list)
     day_happy, max_day_happy = merge_and_find_max(hd_result_list)
